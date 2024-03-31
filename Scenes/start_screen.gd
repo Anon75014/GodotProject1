2c6760ed8ceb122912f00a3296ccf05ec435e70b
xoroux <xoroux@dev.dev>
2024-03-31 21:13:54 +0200

    Reorganising scenes in folders & added new map button

--- Scenes/start_screen.gd
+++ Scenes/start_screen.gd
@@ -7,3 +7,6 @@ func _ready():
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Fight_node_1.tscn")
+
+func _on_play2_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/map.tscn")
