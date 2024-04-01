d9cbd8b035f41e50ad771c65104f2e08de97eef3
xoroux <xoroux@dev.dev>
2024-04-01 22:00:30 +0200

    Added background to menu screens

--- Scenes/Menus/start_screen.gd
+++ Scenes/Menus/start_screen.gd
@@ -1,9 +1,10 @@
 extends CanvasLayer
 
+@onready var play = $"BoxContainer/VBoxContainer/HBoxContainer/PlayButton"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("MarginContainer/VBoxContainer/HBoxContainer/PlayButton").grab_focus()
+	play.grab_focus()
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Maps/Fight_node_1.tscn")
